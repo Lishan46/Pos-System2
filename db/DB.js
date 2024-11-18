70d7374f7ced0d850da46cd00b2bb434104872f2
Lishan <157113673+Lishan46@users.noreply.github.com>
2024-11-18 16:10:51 +0530

    Done Pos System

---
+++ db/DB.js
@@ -0,0 +1,30 @@
+export let Customers = [
+    {
+        custId : 'C01',
+        custName : 'Subodha',
+        custAddress : 'Ambalangod',
+        custSalary : '4000'
+    } ,
+
+    {
+        custId : 'C02',
+        custName : 'Gayani',
+        custAddress : 'Poddala',
+        custSalary : '1000'
+    }
+];
+export let Items = [
+    {
+        itemId : 'I01',
+        itemName : 'SamsugA20',
+        itemQty : 50,
+        itemPrice : 50000
+    },
+    {
+        itemId : 'I02',
+        itemName : 'IPhone 13',
+        itemQty : 10,
+        itemPrice : 175000
+    }
+];
+export let Orders = [];
